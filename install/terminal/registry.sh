087bf34f0d4e0d068383c6ef53033eababf5b9f2
Dani Vela <veladan@me.com>
2024-10-17 13:04:33 +0200

    registry certs

--- install/terminal/registry.sh
+++ install/terminal/registry.sh
@@ -1,5 +1,7 @@
-sudo mkdir -p ~/docker-registry
-sudo mkdir -p ~/docker-registry/data
-sudo cp ~/.local/share/omyserv/configs/registry/docker-compose.yml ~/docker-registry/data/docker-compose.yml
+sudo mkdir -p ~/.docker-registry
+sudo mkdir -p /mnt/raid/registry
+sudo cp ~/.local/share/omyserv/configs/registry/docker-compose.yml ~/.docker-registry/docker-compose.yml
 
-docker compose -f ~/docker-registry/data/docker-compose.yml up -d
+sudo openssl req -x509 -newkey rsa:4096 -keyout ~/.docker-registry/key.pem -out ~/.docker-registry/cert.pem -days 365 -nodes
+
+docker compose -f ~/.docker-registry/docker-compose.yml up -d
