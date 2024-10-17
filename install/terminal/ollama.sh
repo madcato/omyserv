b45231e98dc9cfc2657feb0ae2abaf0330fad087
Dani Vela <veladan@me.com>
2024-10-17 12:40:32 +0200

    compose

--- install/terminal/ollama.sh
+++ install/terminal/ollama.sh
@@ -1,6 +1,11 @@
-curl -fsSL https://ollama.com/install.sh | sh
+if [ -x "$(command -v ollama)" ]; then
+  echo "ollama is alredy installed"
+else
+  curl -fsSL https://ollama.com/install.sh | sh
 
-sudo cp ~/.local/share/omyserv/configs/ollama/ollama.service /etc/systemd/system/ollama.service
+  sudo cp ~/.local/share/omyserv/configs/ollama/ollama.service /etc/systemd/system/ollama.service
 
-sudo systemctl daemon-reload
-sudo systemctl restart ollama
+  sudo systemctl daemon-reload
+  sudo systemctl restart ollama
+fi
+
--- install/terminal/registry.sh
+++ install/terminal/registry.sh
@@ -1,5 +1,5 @@
-sudo mkdir ~/docker-registry
-sudo mkdir ~/docker-registry/data
+sudo mkdir -p ~/docker-registry
+sudo mkdir -p ~/docker-registry/data
 sudo cp ~/.local/share/omyserv/configs/registry/docker-compose.yml ~/docker-registry/data/docker-compose.yml
 
 docker compose -f ~/docker-registry/data/docker-compose.yml up -d
