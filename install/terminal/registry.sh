b5d96d65ba2693070257b332b1c4291a6ef73ad4
Dani Vela <veladan@me.com>
2024-10-17 13:20:15 +0200

    cert

--- install/terminal/registry.sh
+++ install/terminal/registry.sh
@@ -2,6 +2,6 @@ sudo mkdir -p ~/.docker-registry
 sudo mkdir -p /mnt/raid/registry
 sudo cp ~/.local/share/omyserv/configs/registry/docker-compose.yml ~/.docker-registry/docker-compose.yml
 
-sudo openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -days 365 -nodes -subj "/C=ES/ST=Zaragoza/L=Zaragoza/O=veladan.org/OU=software/CN=micro-atx.local"
+sudo openssl req -x509 -newkey rsa:4096 -keyout ~/.docker-registry/key.pem -out ~/.docker-registry/cert.pem -days 365 -nodes -subj "/C=ES/ST=Zaragoza/L=Zaragoza/O=veladan.org/OU=software/CN=micro-atx.local"
 
 docker compose -f ~/.docker-registry/docker-compose.yml up -d
