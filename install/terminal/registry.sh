052c762df40214744dc30c3d03c07e18d9f592d4
Dani Vela <veladan@me.com>
2024-10-18 09:07:31 +0200

    fix certbot

--- install/terminal/registry.sh
+++ install/terminal/registry.sh
@@ -3,6 +3,8 @@ sudo mkdir -p ~/.docker-registry/certs
 sudo mkdir -p /mnt/raid/registry
 sudo cp ~/.local/share/omyserv/configs/registry/docker-compose.yml ~/.docker-registry/docker-compose.yml
 
-sudo openssl req -x509 -nodes -days 365 -newkey rsa:4096 -keyout ~/.docker-registry/certs/key.pem -out r~/.docker-registry/certs/cert.pem  -config ~/.local/share/omyserv/configs/registry/openssl.cnf
+sudo certbot certonly --standalone -d registry.veladan.org
+sudo cp /etc/letsencrypt/live/registry.veladan.org/fullchain.pem .docker-registry/certs/cert.pem
+sudo cp /etc/letsencrypt/live/registry.veladan.org/privkey.pem .docker-registry/certs/key.pem
 
 sudo docker compose -f ~/.docker-registry/docker-compose.yml up -d
